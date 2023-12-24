7f4e026011b18545dbc530273a4ba733dcc271a1
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-12-24 15:17:51 +0200

    modal correction

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,16 +1,21 @@
-// import React from 'react';
 import React, { useEffect } from 'react';
 
 import css from './Modal.module.css';
 
 const Modal = ({ largeImage, onClose }) => {
-  useEffect(() => {
-    const handleKeyDown = e => {
-      if (e.code === 'Escape') {
-        onClose();
-      }
-    };
+  const handleKeyDown = (e) => {
+    if (e.code === 'Escape') {
+      onClose();
+    }
+  };
 
+  const handleOverlayClick = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
     document.addEventListener('keydown', handleKeyDown);
 
     return () => {
@@ -19,11 +24,12 @@ const Modal = ({ largeImage, onClose }) => {
   }, [onClose]);
 
   return (
-    <div className={css.Overlay} onClick={onClose}>
+    <div className={css.Overlay} onClick={handleOverlayClick}>
       <div className={css.Modal}>
-        <img src={largeImage} alt="" />
+        <img src={largeImage} alt="" onClick={(e) => e.stopPropagation()} />
       </div>
     </div>
   );
 };
+
 export { Modal };
